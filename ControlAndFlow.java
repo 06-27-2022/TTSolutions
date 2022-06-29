4cdeff00ec2eef25c4cc7e81b9ff257053d7f036
TMTownes <tabithatownes@gmail.com>
2022-06-29 16:37:21 -0700

    Coding Challenge1: Calculator

---
+++ ControlAndFlow.java
@@ -0,0 +1,60 @@
+public class ControlAndFlow{
+	
+	public static void main(String[] args) {
+		
+		int [] myArr = {95, 85, 62, 40, 79};
+		
+		
+		
+		for (int num : myArr) {
+			
+		
+		
+		if ( num <=59) {
+			System.out.print("History: ");
+			System.out.println("F");
+			System.out.println(" Let's talk!");
+			
+		
+}
+
+		else if ( num >= 60 && num <= 69) {
+			System.out.print("Math: ");
+			System.out.println("D");
+			System.out.println(" You almost got it!");
+			
+}
+		
+		else if ( num >= 70 && num <= 79) {
+			System.out.print("Science: ");
+			System.out.println("C");
+			System.out.println(" C's get degrees!");
+		
+}
+		
+		else if ( num >= 80 && num <= 89) {
+			System.out.print("Art: ");
+			System.out.println("B");
+			System.out.println(" Well done!");
+			
+		
+}
+		else if ( num >= 90 && num <= 100) {
+			System.out.print("English: ");
+			System.out.println("A");
+			System.out.println(" Excellent!");
+			
+		
+}
+}
+	}
+	}
+
+
+
+		
+
+	
+		
+		
+
